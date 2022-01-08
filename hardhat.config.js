aa86d904dfcffa719e6a79a54c576c99548cb035
Harsh8055 <harshsingh.ytbusiness@gmail.com>
2022-01-08 20:57:22 +0530

    get functions added

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,12 +8,15 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    ropsten: {
-      url: process.env.INFURA_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    hardhat: {
+      chainId: 1337
     },
+    /*ropsten: {
+      url: process.env.INFURA_URL,
+      accounts: [0x${process.env.PRIVATE_KEY}]
+    }*/
   },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  /*etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }*/
 };
